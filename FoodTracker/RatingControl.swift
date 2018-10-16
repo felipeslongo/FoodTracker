2e976806badc20813607531128619d94a9cd0cc0
Felipe de Souza Longo <felipeslongo@gmail.com>
2018-10-15 22:54:04 -0300

    Checkpoint: Run your app. You should see five stars and be able to click one to change the rating. Click the third star to change the rating to 3, for example. Click the same star a second time. The control should reset to a zero-star rating.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -22,7 +22,11 @@ import UIKit
         }
     }
     private var ratingButtons = [UIButton]()
-    var rating = 0
+    var rating = 0 {
+        didSet{
+            updateButtonSelectionStates()
+        }
+    }
 
     //MARK: Initialization
     override init(frame: CGRect) {
@@ -36,8 +40,21 @@ import UIKit
     }
     
     //MARK: Button Action
-    @objc func ratingButtonTapped(button: UIButton){
-        print("Button pressed 👍")
+    @objc func ratingButtonTapped(button: UIButton) {
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // Calculate the rating of the selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
     }
     
     //MARK: Private Methods
@@ -79,6 +96,15 @@ import UIKit
             
             //Add button to the array
             ratingButtons.append(button)
+        }
+        
+        updateButtonSelectionStates()
+    }
+    
+    private func updateButtonSelectionStates(){
+        for (index, button) in ratingButtons.enumerated(){
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
         }
     }
 }
