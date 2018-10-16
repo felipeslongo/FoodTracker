1fb9063e0deebab6c723bc28ba6a56dc2cb331d3
Felipe de Souza Longo <felipeslongo@gmail.com>
2018-10-15 21:25:37 -0300

    Checkpoint: Run your app again. Now, the buttons should be laid out as expected. Note, clicking any of the buttons at this point should still call the ratingButtonTapped(button:) method and log the message to the console.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class RatingControl: UIStackView {
+    
+    //MARK: Properties
+    private var ratingButtons = [UIButton]()
+    
+    var rating = 0
 
     //MARK: Initialization
     override init(frame: CGRect) {
@@ -29,19 +34,24 @@ class RatingControl: UIStackView {
     //MARK: Private Methods
     private func settupButtons(){
         
-        //Inicialize button
-        let button = UIButton()
-        button.backgroundColor = UIColor.red
-        
-        //Add constraints.
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
-        
-        //Setup the button action
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
-        
-        //Add button to the stack
-        addArrangedSubview(button)
+        for _ in 0..<5{
+            //Inicialize button
+            let button = UIButton()
+            button.backgroundColor = UIColor.red
+            
+            //Add constraints.
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            
+            //Setup the button action
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
+            
+            //Add button to the stack
+            addArrangedSubview(button)
+            
+            //Add button to the array
+            ratingButtons.append(button)
+        }
     }
 }
