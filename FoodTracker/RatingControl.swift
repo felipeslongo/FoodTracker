50cfd73540f61dc97fdc93d4ca33c04e8362c7a2
Felipe de Souza Longo <felipeslongo@gmail.com>
2018-10-15 21:49:23 -0300

    Checkpoint: Open Main.storyboard and select the RatingControl object. Try changing the Star Size and Star Count attributes. The control in the canvas should change to match the new settings. Run the app, and you should see the changes in the simulator.

--- FoodTracker/RatingControl.swift
+++ FoodTracker/RatingControl.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class RatingControl: UIStackView {
+@IBDesignable class RatingControl: UIStackView {
     
     //MARK: Properties
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0){
+        didSet{
+            settupButtons()
+        }
+    }
+    @IBInspectable var starCount: Int = 5{
+        didSet{
+            settupButtons()
+        }
+    }
     private var ratingButtons = [UIButton]()
-    
     var rating = 0
 
     //MARK: Initialization
@@ -34,15 +43,21 @@ class RatingControl: UIStackView {
     //MARK: Private Methods
     private func settupButtons(){
         
-        for _ in 0..<5{
+        for button in ratingButtons{
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
+        
+        for _ in 0..<starCount{
             //Inicialize button
             let button = UIButton()
             button.backgroundColor = UIColor.red
             
             //Add constraints.
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             //Setup the button action
             button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(button:)), for: .touchUpInside)
